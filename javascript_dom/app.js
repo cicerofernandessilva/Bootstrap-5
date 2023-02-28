f1c4aae8f1d1f5c555c2ec47e125e95dc4470a7d
cicerofernandessilva <cicerofernandessilva@gmail.com>
2023-02-28 01:35:35 -0300

    Javascript-dom
    Conecendo el DOM

---
+++ javascript_dom/app.js
@@ -0,0 +1,8 @@
+console.log("funciona! 😍");
+
+console.log(document);
+console.log(document.head);
+console.log(document.title);
+console.log(document.getElementById("textoWeb"));
+console.log(document.getElementById("textoWeb").textContent);
+console.log(document.getElementById("textoWeb").innerHTML);
