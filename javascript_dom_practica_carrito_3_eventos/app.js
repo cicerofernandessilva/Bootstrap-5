07e37176c0a6803cb05cfdd3cd46f3442197e29e
cicerofernandessilva <cicerofernandessilva@gmail.com>
2023-03-10 00:07:50 -0300

    retirado código de otros testes

--- javascript_dom_practica_carrito_3_eventos/app.js
+++ javascript_dom_practica_carrito_3_eventos/app.js
@@ -1,123 +1,49 @@
 // // console.log("funciona!");
 
-// const carrito = document.querySelector("#carrito");
-// const template = document.querySelector("#template");
-// const btnAgregar = document.querySelectorAll(".card .btn");
-// const fragment = document.createDocumentFragment();
+const carrito = document.querySelector("#carrito");
+const template = document.querySelector("#template");
+const btnAgregar = document.querySelectorAll(".card .btn");
+const fragment = document.createDocumentFragment();
 
-// // //teste
-// // console.log(carrito);
-// // console.log(template);
-// // console.log(btnAgregar);
-// // console.log(fragment);
+// //teste
+// console.log(carrito);
+// console.log(template);
+// console.log(btnAgregar);
+// console.log(fragment);
 
-// const carritoObj = [];
+const carritoObj = [];
 
-// const agrCarrito = (e) => {
-//   //   console.log(e.target);
-//   console.log(e.target.dataset.fruta);
-//   const producto = {
-//     titulo: e.target.dataset.fruta,
-//     id: e.target.dataset.fruta,
-//     cantidad: 1,
-//   };
+const agrCarrito = (e) => {
+  //   console.log(e.target);
+  console.log(e.target.dataset.fruta);
+  const producto = {
+    titulo: e.target.dataset.fruta,
+    id: e.target.dataset.fruta,
+    cantidad: 1,
+  };
 
-//   const indice = carritoObj.findIndex((item) => item.id === producto.id);
-//   // console.log(indice);
+  const indice = carritoObj.findIndex((item) => item.id === producto.id);
+  // console.log(indice);
 
-//   if (indice === -1) {
-//     carritoObj.push(producto);
-//   } else {
-//     carritoObj[indice].cantidad++;
-//   }
-//   // console.log(carritoObj);
-
-//   pintarCarrito(carritoObj);
-// };
-
-// const pintarCarrito = (array) => {
-//   carrito.textContent = "";
-//   //   console.log("pintar carrito", producto);
-//   carritoObj.forEach((item) => {
-//     const clone = template.content.firstElementChild.cloneNode(true);
-//     clone.querySelector(".lead").textContent = item.titulo;
-//     clone.querySelector(".badge").textContent = item.cantidad;
-//     fragment.appendChild(clone);
-//   });
-//   carrito.appendChild(fragment);
-// };
-// btnAgregar.forEach((btn) => btn.addEventListener("click", agrCarrito));
-
-//teste de eventos
-
-// const padre = document.querySelector(".border-primary");
-// const hijo = document.querySelector(".border-secondary");
-// const nieto = document.querySelector(".border-danger");
-// console.log(padre);
-// console.log(hijo);
-// console.log(nieto);
-// propagacion del evento
-// padre.addEventListener("click", () => console.log("Me desti click - padre "));
-// hijo.addEventListener("click", () => console.log("Me desti click - hijo "));
-// nieto.addEventListener("click", () => console.log("Me desti click - nieto "));
-
-// con forEach
-
-// const all = document.querySelectorAll(".border");
-
-// all.forEach((propagacion) => {
-//   propagacion.addEventListener("click", () => console.log("Me deste click"));
-// });
-
-//sem propagação do evento
-
-// all.forEach((propagacion) => {
-//   propagacion.addEventListener("click", (e) => {
-//     e.stopPropagation();
-//     console.log("Me deste click");
-//   });
-// });
-
-// manipulando formulario
-
-const form = document.querySelector("form");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log("Me deste click");
-});
-
-//con ancla
-
-const ancla = document.querySelector("a");
-
-ancla.addEventListener("click", (e) => console.log("Me deste click!"));
-
-// Delegacion de eventos
-
-//manipulando eventos del DOM com elemento padres
-
-const padre = document.querySelector(".container");
-// console.log("div container", padre);
-
-padre.addEventListener("click", (e) => {
-  // console.log(e.target);
-  // console.log(e.target.id);
-  if (e.target.id === "padre") {
-    console.log("Diste click al Padre");
+  if (indice === -1) {
+    carritoObj.push(producto);
+  } else {
+    carritoObj[indice].cantidad++;
   }
-  if (e.target.id === "hijo") {
-    console.log("Diste click al Hijo");
-  }
-  if (e.target.id === "nieto") {
-    console.log("Diste click al Nieto");
-  }
+  // console.log(carritoObj);
 
-  if (e.target.matches(".border-secondary")) {
-    console.log("Diste click al Hijo - uso do matches()");
-  }
+  pintarCarrito(carritoObj);
+};
 
-  if (e.target.dataset.div === "divNieto") {
-    console.log("Diste click al Nieto", e.target.dataset.div);
-  }
-});
+const pintarCarrito = (array) => {
+  carrito.textContent = "";
+  //   console.log("pintar carrito", producto);
+  carritoObj.forEach((item) => {
+    const clone = template.content.firstElementChild.cloneNode(true);
+    clone.querySelector(".lead").textContent = item.titulo;
+    clone.querySelector(".badge").textContent = item.cantidad;
+    fragment.appendChild(clone);
+  });
+  carrito.appendChild(fragment);
+};
+btnAgregar.forEach((btn) => btn.addEventListener("click", agrCarrito));
