dbc4be71f4e29a0266578459a6f5a967cf1af301
cicerofernandessilva <cicerofernandessilva@gmail.com>
2023-03-17 00:31:51 -0300

    Javascript_fetch_practica
    Aprendendo fetch API

--- javascript_fetch_practica/app.js
+++ javascript_fetch_practica/app.js
@@ -1,1 +1,7 @@
-console.log("Funciona!");
+// console.log("Funciona!");
+
+const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => console.log(data));
