1209a6a8a670ab9e2afea34728911289b2ca5f13
cicerofernandessilva <cicerofernandessilva@gmail.com>
2023-04-14 00:59:54 -0300

    aprendendo sobre package.json

--- node_js_mi_premera_app/frutas.js
+++ node_js_mi_premera_app/frutas.js
@@ -1,3 +1,3 @@
-const frutas = ["🍉", "🍓", "🍋"];
+const frutas = ["🍉", "🍓", "🍋", "🍎", "🍊"];
 const precios = [100, 200, 300];
 module.exports = { frutas, precios }; // exportando elementos do arquivo atual
