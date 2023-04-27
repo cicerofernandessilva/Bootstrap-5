6503c1fb706520c8091b77864bdc1d84155bfa75
cicerofernandessilva <cicerofernandessilva@gmail.com>
2023-04-27 01:57:23 -0300

    Estudando node.js
    aprendendo sobre middleware, uso do get e post y como leer los datos.
    uso del fileSyaten para crear archivos txt

--- node_js_mi_primer_vista_con_express/index.js
+++ node_js_mi_primer_vista_con_express/index.js
@@ -9,17 +9,35 @@
 // server.listen(port, () => console.log("Funcionando! 😁 "));
 
 //uso de modulos nativos do express
-
+const fs = require("fs");
 const express = require("express");
+const { clearScreenDown } = require("readline");
 
 const app = express();
 const port = 5000;
 
 //middleware
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true })); // necessário para ler informações capturadas via post
 
-app.get("/formulario", (req, res) => {
-  res.send("Formulário enviado!...");
+// app.get("/formulario", (req, res) => {
+//   console.log(req.query); //mostrar a informação capturada
+//   res.send("Formulário enviado!...");
+// });
+
+app.post("/formulario", (req, res) => {
+  console.log(req.body); //mostrar a informação capturada via post
+  const { nombre, apellido } = req.body; // coloca os dados dentro de uma constante
+  // if (!nombre || !apellido) return res.send("No colocaste tus datos!"); //texto em tela
+  if (!nombre || !apellido) return res.redirect("/erro.html"); //cria pagina de erro
+  console.log(nombre, apellido);
+  //crear archivos con fs
+  fs.writeFile(`archivo/${nombre}.txt`, apellido, (err) => {
+    if (err) return res.send("Falló al crear el archivo!");
+    // res.send("Se creo el archivo!");
+    res.download(__dirname + `/archivo/${nombre}.txt`);
+  });
+  // res.send("Formulário enviado!...");
 });
 
 app.get("/", (req, res) => {
