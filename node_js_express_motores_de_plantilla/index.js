3668b52f7210afe102f949a126ce1f70fad8a469
cicerofernandessilva <cicerofernandessilva@gmail.com>
2023-04-28 01:36:10 -0300

    creacion del motores de plantilla

--- node_js_express_motores_de_plantilla/index.js
+++ node_js_express_motores_de_plantilla/index.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const { create } = require("express-handlebars");
 const app = express();
+const hbs = create({ extname: "hbs" });
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", ".hbs");
+app.set("views", "./views");
 
 app.get("/", (req, res) => {
-  res.send("Estas solicitando la ruta raiz😍!");
+  // res.send("Estas solicitando la ruta raiz😍!");
+  res.render("home");
 });
 
+app.use(express.static(__dirname + "/public"));
 app.listen(5000, () => console.log("Servidor andando👌!"));
